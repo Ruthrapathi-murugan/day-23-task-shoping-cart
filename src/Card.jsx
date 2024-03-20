52f32cfad2beff91a220d1a379e9cafa1a221f00
Ruthrapathi-murugan <153907596+Ruthrapathi-murugan@users.noreply.github.com>
2024-03-20 09:25:26 +0530

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,15 +5,15 @@ function Card() {
     <>
       <div className="card-body p-4">
         <div className="card h-100">
-          <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+          <img className="card-img-top" src="https://picsum.photos/200/300  " alt="..." />
           <div className="card-body p-4">
             <div className="text-center">
               <h5 className="fw-bolder">Fancy Product</h5>
               $40.00 - $80.00
             </div>
           </div>
-          <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-            <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+          <div className="card-footer p-4 pt-0 border-top-0 bg-transparent" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <button type="button" className="btn btn-dark">Add to cart</button>
           </div>
         </div>
       </div>
