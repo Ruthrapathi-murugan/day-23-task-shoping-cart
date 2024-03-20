3a0fbe569224752f3d42797f2efee5c2ffc95eff
Ruthrapathi-murugan <153907596+Ruthrapathi-murugan@users.noreply.github.com>
2024-03-20 09:25:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,33 @@
-import './App.css'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './navbar'
-import Cartbuton from './Cartbuton';
+import Navbar from './Navbar';
 import Header from './Header';
-import Card from './Card'
-function App() {
+import Card from './Card';
 
+function App() {
   return (
     <>
-    <Navbar/>
-    <div className="cartbut" style={{padding:'20px' ,justifyContent:'right'}}>
-     <Cartbuton/>
-     </div>
-     <Header/>
-    <div className="id"  style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px', gap: '20px' }}>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    </div>
+      <Navbar />
+      <div className="cartbut" style={{ padding: '20px', justifyContent: 'right' }}></div>
+      <Header />
+      <div className="container">
+      <div className="row">
+      <div className="id" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px', gap: '20px', flexWrap: 'wrap' }}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card /> 
+      </div>
+      </div>
+      </div>
+     
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
